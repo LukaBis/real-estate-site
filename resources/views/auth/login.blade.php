45a850a1285621a0f1ea969f584a059aa909a529
LukaBis <lukabiskupi@yahoo.com>
2021-09-23 16:07:59 +0200

    login validation messages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,6 +45,15 @@
 	            	</div>
 	            </div>
 	          </form>
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
 	        </div>
 				</div>
 			</div>
