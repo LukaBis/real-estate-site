f626146297f555309241ea910d6fbdaed7d89939
LukaBis <lukabiskupi@yahoo.com>
2021-09-28 23:03:54 +0200

    add property done

--- app/Http/Controllers/AddPropertyController.php
+++ app/Http/Controllers/AddPropertyController.php
@@ -132,6 +132,6 @@ class AddPropertyController extends Controller
           ]);
         }
 
-        return "Saved";
+        return redirect('home/property/'.$newProperty->id)->with('successMessage', 'Saved successfully');
     }
 }
